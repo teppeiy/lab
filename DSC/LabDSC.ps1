7410910659797b0e5d17824197aa6400c732ea6d
Teppei Yamashita <teppeiy@microsoft.com>
2017-12-27 16:30:55 +0900

    ADFS DSC Support

--- DSC/LabDSC.ps1
+++ DSC/LabDSC.ps1
@@ -141,3 +141,158 @@ configuration Forest {
 
     }
 }
+
+configuration ADFS {
+    param
+    (
+        [Parameter(Mandatory)] 
+        [pscredential]$domainCred
+    )
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine -Force
+    Import-DscResource -Module PSDesiredStateConfiguration, xActiveDirectory, xComputerManagement
+
+    Node 'localhost'
+    {
+        LocalConfigurationManager {
+            DebugMode          = 'All'
+            RebootNodeIfNeeded = $true
+        }
+        xComputer JoinDomain
+        {
+            Name          = $MachineName 
+            DomainName    = $ConfigurationData.NonNodeData.DomainName 
+            Credential    = $domainCred  # Credential to join to domain
+        }
+        WindowsFeature installADFS  #install ADFS
+        {
+            Ensure = "Present"
+            Name   = "ADFS-Federation"
+        }
+        <#
+        WindowsFeature RSAT-AD-AdminCenter {
+            Ensure = "Present"
+            Name   = "RSAT-AD-AdminCenter"
+        }
+        WindowsFeature RSAT-ADDS {
+            Ensure = "Present"
+            Name   = "RSAT-ADDS"
+        }
+        WindowsFeature RSAT-AD-PowerShell {
+            Ensure = "Present"
+            Name   = "RSAT-AD-PowerShell"
+        }
+        WindowsFeature RSAT-AD-Tools {
+            Ensure = "Present"
+            Name   = "RSAT-AD-Tools"
+        }
+       #>
+        foreach ($m in @($ConfigurationData.NonNodeData.PowerShellModules)) {
+            Script $m {
+                SetScript  = {
+                    Install-Module -Name $using:m -AllowClobber -Force
+                }
+
+                GetScript  = { @{} }
+                TestScript = { 
+                    $key = Get-Module -Name $using:m -ListAvailable
+                    return ($key -ine $null)
+                }
+            }
+        }
+
+        Script DeployLinks {
+            SetScript  = {
+                $WshShell = New-Object -comObject WScript.Shell
+                $dt = "C:\Users\Public\Desktop\"
+                $links = @(
+                    @{site = "%windir%\system32\WindowsPowerShell\v1.0\PowerShell_ISE.exe"; name = "PowerShell ISE"; icon = "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell_ise.exe, 0"},
+                    @{site = "%SystemRoot%\system32\dsa.msc"; name = "AD Users and Computers"; icon = "%SystemRoot%\system32\dsadmin.dll, 0"},
+                    @{site = "%SystemRoot%\system32\domain.msc"; name = "AD Domains and Trusts"; icon = "%SystemRoot%\system32\domadmin.dll, 0"},
+                    @{site = "%SystemRoot%\system32\dnsmgmt.msc"; name = "DNS"; icon = "%SystemRoot%\system32\dnsmgr.dll, 0"},
+                    @{site = "%windir%\system32\services.msc"; name = "Services"; icon = "%windir%\system32\filemgmt.dll, 0"}
+                )
+
+                foreach ($link in $links) {
+                    $Shortcut = $WshShell.CreateShortcut("$($dt)$($link.name).lnk")
+                    $Shortcut.TargetPath = $link.site
+                    $Shortcut.IconLocation = $link.icon
+                    $Shortcut.Save()
+                }
+            }
+            GetScript  = { @{} }
+            TestScript = {
+                $icons = Get-ChildItem -Path "C:\Users\Public\Desktop\"
+                $result = 0
+                foreach ($i in $icons) {
+                    if (($i.name).endswith('.lnk')) {
+                        $result++
+                    }
+                }
+                return ($result -gt 0)
+            }
+        }
+        Script InstallAzureADConnect {            
+            GetScript  = { @{} }
+            SetScript  = 
+            {
+                $MSIPath = "C:\Users\Public\Downloads\AzureADConnect.msi"
+                Invoke-WebRequest -Uri "https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi" -OutFile $MSIPath
+                Invoke-Expression "& $env:SystemRoot\system32\msiexec.exe /i $MSIPath /qn /passive /forcerestart"
+            }
+
+            TestScript = 
+            {
+                return ((Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | where {$_.DisplayName -eq 'Microsoft Azure AD Connect'}) -ine $null)
+            }
+        }
+
+    }
+}
+
+# Configuration Data for AD
+$ConfigData = @{ 
+    AllNodes    = @( 
+        @{ 
+            Nodename                    = "localhost" 
+            Role                        = "DC" 
+            #DomainName = "teppeiy.local"
+            RetryCount                  = 20  
+            RetryIntervalSec            = 30  
+            PsDscAllowPlainTextPassword = $true
+        }, 
+        @{ 
+            Nodename         = "adfs" 
+            Role             = "ADFS" 
+            #DomainName = "teppeiy.local" 
+            RetryCount       = 20  
+            RetryIntervalSec = 30  
+            #PsDscAllowPlainTextPassword = $true
+        } 
+    )
+    NonNodeData = @{
+        PowerShellModules   = 'MSOnline', 'AzureAD', 'AzureADPreview'
+        DomainName          = 'teppeiy.local'
+        AdGroups            = 'HR', 'Sales', 'IT', 'VIP'
+        OrganizationalUnits = 'FTE', 'Clients'
+ 
+        AdUsers             = @(
+            @{
+                FirstName         = 'User1' 
+                LastName          = '(teppeiy.local)'
+                UserName          = 'user1' #SamAccountName
+                UserPrincipalName = 'user1@teppeiy.local'
+                Department        = 'Sales'
+                OU                = 'FTE'
+                Title             = 'Sales Manager' 
+            }
+            @{
+                FirstName  = 'User2' 
+                LastName   = '(teppeiy.local)'
+                UserName   = 'user2' #SamAccountName
+                Department = 'HR'
+                OU         = 'FTE'
+                Title      = 'HR Manager' 
+            }
+        ) 
+    }
+}
