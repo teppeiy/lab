3207686f2077701e86d98803185921975bfccf64
Teppei Yamashita <teppeiy@microsoft.com>
2017-12-29 10:57:17 +0900

    zip Commit

---
+++ DSC-Test/DSC-InstallAzureADConnect.ps1
@@ -0,0 +1,29 @@
+﻿Configuration DSCBlockTest
+{
+    Import-DscResource -Module xPSDesiredStateConfiguration
+    Node "localhost"
+    {
+        xRemoteFile DownloadAzureADConnect {
+            Uri = "https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi"
+            DestinationPath = "C:\Users\Public\Downloads\AzureADConnect.msi"
+        }
+        Script InstallAzureADConnect {            
+            GetScript  = { @{} }
+            SetScript  = 
+            {
+                $MSIPath = "C:\Users\Public\Downloads\AzureADConnect.msi"
+                Invoke-Expression "& $env:SystemRoot\system32\msiexec.exe /i $MSIPath /qn /passive /forcerestart"
+            }
+            TestScript = 
+            {
+                return ((Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | where {$_.DisplayName -eq 'Microsoft Azure AD Connect'}) -ine $null)
+            }
+            DependsOn = "[xRemoteFile]DownloadAzureADConnect"
+        }
+    }
+}
+
+$outputPath = "C:\DSC\DSCBlockTest"
+DSCBlockTest -OutputPath $outputPath 
+
+Start-DscConfiguration -Path $outputPath -Wait -Verbose
