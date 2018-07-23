4bc8c250217e18e556fccef897c8b8e74f7ec566
Teppei Yamashita <teppeiy@microsoft.com>
2018-07-23 10:04:01 +0900

    1

--- DSC-Test/Hello-DSC.ps1
+++ DSC-Test/Hello-DSC.ps1
@@ -4,28 +4,9 @@
     Import-DscResource -Module xPSDesiredStateConfiguration
     Node "localhost"
     {
-        LocalConfigurationManager            
-        {            
-            DebugMode = 'All'
-            ActionAfterReboot = 'ContinueConfiguration'                    
-            RebootNodeIfNeeded = $false
-        }
-        xRemoteFile DownloadADFS {
-            Uri = "https://download.microsoft.com/download/F/3/D/F3D66A7E-C974-4A60-B7A5-382A61EB7BC6/RTW/W2K8R2/amd64/AdfsSetup.exe"
-            DestinationPath = "C:\Users\Public\Downloads\AzureADConnect.msi"
-        }
-        Script InstallADFS{            
-            GetScript  = { @{} }
-            SetScript  = 
-            {
-                $MSIPath = "C:\Users\Public\Downloads\AzureADConnect.msi"
-                Invoke-Expression "& $env:SystemRoot\system32\msiexec.exe /i $MSIPath /qn /passive /forcerestart"
-            }
-            TestScript = 
-            {
-                return ((Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | where {$_.DisplayName -eq 'Microsoft Azure AD Connect'}) -ine $null)
-            }
-            DependsOn = "[xRemoteFile]DownloadADFS"
+        WindowsFeature ADDSInstall { 
+            Ensure = "Present" 
+            Name   = "AD-Domain-Services"
         }
     }
 }
