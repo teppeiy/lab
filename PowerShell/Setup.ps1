3940d5607966e77c42d400200f22fe44a03b9805
Teppei Yamashita <teppeiy@microsoft.com>
2018-07-24 20:42:24 +0900

    1

---
+++ PowerShell/Setup.ps1
@@ -0,0 +1,5 @@
+Install-Module -Name xPSDesiredStateConfiguration -Force
+Install-Module -Name xActiveDirectory -Force
+Install-Module -Name xComputerManagement -Force
+Install-Module -Name xPendingReboot -Force
+Install-Module -Name xWindowsUpdate -Force
