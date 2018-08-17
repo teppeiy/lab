05e304141f66ae6814ee74bbf64d000ad00ea034
Teppei Yamashita <teppeiy@microsoft.com>
2018-08-17 14:53:48 +0900

    1

--- Apply-DSC2VM.ps1
+++ Apply-DSC2VM.ps1
@@ -2,6 +2,6 @@
 $ResourceGroupName = "lab5"
 $VmName = "lab5-dc"
 
-Publish-AzureRmVMDscConfiguration -ConfigurationPath ".\DSC\LabDSC\LabDSC.ps1" -ConfigurationDataPath ".\DSC\LabDSC\LabDSC.psd1" -SkipDependencyDetection -ResourceGroupName "Utilities" -StorageAccountName "teppeiy" -Force
+Publish-AzureRmVMDscConfiguration -ConfigurationPath ".\DSC\LabDSC\LabDSC.ps1" -ConfigurationDataPath ".\DSC\LabDSC\LabDSC.psd1" -ResourceGroupName "Utilities" -StorageAccountName "teppeiy" -Force
 
 Set-AzureRmVMDscExtension -ResourceGroupName $ResourceGroupName -VMName $VmName -ArchiveBlobName "LabDSC.ps1.zip" -ArchiveResourceGroupName "Utilities" -ArchiveStorageAccountName "teppeiy" -ConfigurationName "DC" -Version "2.76"
--- DSC/ArchiveModules.ps1
+++ DSC/ArchiveModules.ps1
@@ -1,5 +1,5 @@
 #Compress-Archive -Path .\LabDSC\* -Update -DestinationPath .\LabDSC.ps1.zip
 
-Publish-AzureRmVMDscConfiguration -ConfigurationPath ".\LabDSC\LabDSC.ps1" -ConfigurationDataPath ".\LabDSC\LabDSC.psd1" -SkipDependencyDetection -OutputArchivePath ".\LabDSC.ps1.zip" -Force
+Publish-AzureRmVMDscConfiguration -ConfigurationPath ".\LabDSC\LabDSC.ps1" -ConfigurationDataPath ".\LabDSC\LabDSC.psd1" -OutputArchivePath ".\LabDSC.ps1.zip" -Force
 
 
