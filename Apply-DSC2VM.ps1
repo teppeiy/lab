04a9ef305cdd3143d84aadfb23790fb7dd602bf8
Teppei Yamashita <teppeiy@microsoft.com>
2018-08-17 16:28:37 +0900

    1

--- Apply-DSC2VM.ps1
+++ Apply-DSC2VM.ps1
@@ -1,7 +1,29 @@
-
-$ResourceGroupName = "lab5"
-$VmName = "lab5-dc"
-
 Publish-AzureRmVMDscConfiguration -ConfigurationPath ".\DSC\LabDSC\LabDSC.ps1" -ConfigurationDataPath ".\DSC\LabDSC\LabDSC.psd1" -ResourceGroupName "Utilities" -StorageAccountName "teppeiy" -Force
 
-Set-AzureRmVMDscExtension -ResourceGroupName $ResourceGroupName -VMName $VmName -ArchiveBlobName "LabDSC.ps1.zip" -ArchiveResourceGroupName "Utilities" -ArchiveStorageAccountName "teppeiy" -ConfigurationName "DC" -Version "2.76"
+$ResourceGroupName = "lab9"
+$VmName = "lab9-dc"
+
+$cred = Get-Credential
+$configurationArguments = @{
+    domainName = "teppeiy.local"
+    safemodeAdministratorCred = $cred
+    domainCred = $cred
+    NewADUserCred = $cred
+}
+
+Set-AzureRmVMDscExtension -ResourceGroupName $ResourceGroupName -VMName $VmName -ArchiveBlobName "LabDSC.ps1.zip" -ConfigurationArgument $configurationArguments -ArchiveResourceGroupName "Utilities" -ArchiveStorageAccountName "teppeiy" -ConfigurationName "DC" -Version "2.76"
+
+#################################################
+# FS
+#################################################
+$ResourceGroupName = "lab5"
+$VmName = "lab5-fs"
+
+$cred = Get-Credential
+$configurationArguments = @{
+    domainName = "teppeiy.local"
+    domainCred = $cred
+}
+
+Set-AzureRmVMDscExtension -ResourceGroupName $ResourceGroupName -VMName $VmName -ArchiveBlobName "LabDSC.ps1.zip" -ConfigurationArgument $configurationArguments -ArchiveResourceGroupName "Utilities" -ArchiveStorageAccountName "teppeiy" -ConfigurationName "FS" -Version "2.76"
+
